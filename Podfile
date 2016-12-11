d88eca9c841d6a8deacae798bdef4286af474375
Madelyn Nelson <mnelso12@nd.edu>
2016-12-10 20:54:47 -0500

    first commit

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+#
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+
+target 'crescat' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for crescat
+
+  target 'crescatTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'crescatUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- crescat/ViewController.swift
+++ crescat/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var logoutButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let user = FIRAuth.auth()?.currentUser {
+            self.logoutButton.alpha = 1.0
+            self.userLabel.text = user.email
+        }
+        else {
+            self.logoutButton.alpha = 0.0
+            self.userLabel.text = ""
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,119 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func createAccountAction(_ sender: Any) {
+        if self.emailField.text == "" || self.passwordField.text == ""
+        {
+            let alertController = UIAlertController(title: "Oops!", message: "Please enter a username and password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else
+        {
+            // create account
+            FIRAuth.auth()?.createUser(withEmail: self.emailField.text!, password: self.passwordField.text!, completion: { (user, error) in
+                if error == nil
+                {
+                    // successfully created user
+                    /*
+                    self.logoutButton.alpha = 1.0
+                    self.userLabel.text = user?.email
+                    */
+                    self.userLabel.text = "waiting for email verification"
+                    self.emailField.text = ""
+                    self.passwordField.text = ""
+                    
+                    NSLog("sending verification email")
+                    user?.sendEmailVerification(completion: nil)
+                    try! FIRAuth.auth()?.signOut()
+                }
+                else
+                {
+                    // failed to create user
+                    let alertController = UIAlertController(title: "Oops!", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            })
+        }
+    }
 
+    @IBAction func logoutAction(_ sender: Any) {
+        try! FIRAuth.auth()?.signOut()
+        self.userLabel.text = ""
+        self.logoutButton.alpha = 0.0
+        self.emailField.text = ""
+        self.passwordField.text = ""
+    }
+    
+    @IBAction func loginAction(_ sender: Any) {
+        if self.emailField.text == "" || self.passwordField.text == ""
+        {
+            let alertController = UIAlertController(title: "Oops!", message: "Please enter a username and password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else
+        {
+            
+            FIRAuth.auth()?.signIn(withEmail: self.emailField.text!, password: self.passwordField.text!, completion: { (user, error) in
+                if error == nil
+                {
+                    // successfully logged in
+                    //self.userLabel.text = "signed in, checking for email verification..."
+                    
+                    // check for email verification
+                    if let user = FIRAuth.auth()?.currentUser {
+                        if !user.isEmailVerified
+                        {
+                            NSLog("email is NOT verified");
+                            let alertVC = UIAlertController(title: "Error", message: "Sorry. Your email address has not yet been verified. Do you want us to send another verification email to \(self.emailField.text).", preferredStyle: .alert)
+                            let alertActionOkay = UIAlertAction(title: "Okay", style: .default) {
+                                (_) in
+                                user.sendEmailVerification(completion: nil)
+                            }
+                            let alertActionCancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+                            
+                            alertVC.addAction(alertActionOkay)
+                            alertVC.addAction(alertActionCancel)
+                            self.present(alertVC, animated: true, completion: nil)
+                            
+                            // sign them out because email not verified
+                            try! FIRAuth.auth()?.signOut()
+                        }
+                        else
+                        {
+                            NSLog("Email verified. Signing in...")
+                            self.logoutButton.alpha = 1.0
+                            self.userLabel.text = user.email
+                            self.emailField.text = ""
+                            self.passwordField.text = ""
+
+                        }
+                    }
+                    
+                    
+                }
+                else
+                {
+                    // failed to login
+                    let alertController = UIAlertController(title: "Oops!", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+
+            })
+ 
+
+        }
+    }
 }
 
