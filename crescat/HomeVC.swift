16999f35277d8c6f089fd67e266a60fec4b1c02f
Madelyn Nelson <mnelso12@nd.edu>
2017-01-04 18:17:02 -0600

    newsfeed corresponds to followed profs

--- crescat/HomeVC.swift
+++ crescat/HomeVC.swift
@@ -19,16 +19,15 @@ class HomeVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     
     var professionalArray: [[String:AnyObject]] = []
+    var questionArray: [[String:AnyObject]] = []
+    var followeesArray: [String] = []
     
-    // These strings will be the data for the table view cells
+    /*
     let titles: [String] = ["Bernie Sanders", "Alexandria Viegut", "Barack Obama", "Queen Elizabeth II"]
     let questions: [String] = ["Q: Why did you choose deloitte?", "Q: What is your proudest achievement?", "Q: What is the best advice you've ever received?", "Q: Why Notre Dame?"]
     let answers: [String] = ["A: I really wanted to do consulting, it sounds super fun and awesome and they pay really well and I love being social and food!", "A: I went skydiving once and it was awesome, conquered ALL the fears and it was great bonding!", "A: Go to college, don't do drugs.", "A: Because Notre Dame has the best community ever, duh!"]
     let dates: [String] = ["1/2/17", "12/13/16", "12/1/16", "11/15/16"]
-    
-    // These are the colors of the square views in our table view cells.
-    // In a real project you might use UIImages.
-    let colors = [UIColor.blue, UIColor.yellow, UIColor.magenta, UIColor.red, UIColor.brown]
+    */
     
     // Don't forget to enter this in IB also
     let cellReuseIdentifier = "cell"
@@ -37,6 +36,10 @@ class HomeVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // call firebase to get the Q/A of profs that this user is following
+        //getListOfFollowees() no called from viewDidAppear
+        //getQuestionsAndAnswers() now called from getListOfFollowees
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -49,7 +52,90 @@ class HomeVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         let user = FIRAuth.auth()?.currentUser
         self.userEmailLabel.text = user?.email
         
-        getProfessionalsList()
+        getProfessionalsList() // gets full list of professionals for searching
+    }
+    
+    
+    // need this here in order to reload tableview after switching prof followees
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+       print("VIEW APPEARING!")
+
+        getListOfFollowees()
+        tableView.reloadData()
+    }
+    
+    
+    func getListOfFollowees() {
+        followeesArray = [] // reset list of followees
+        
+        let userID = FIRAuth.auth()?.currentUser?.uid
+        let userPath = "users/" + userID!
+        let usersRef = FIRDatabase.database().reference(withPath: userPath)
+        
+        usersRef.observe(.childAdded, with: { snapshot in
+            print(snapshot.value)
+            
+            if (snapshot.hasChild("listOfFollowees")) {
+                
+                let json = snapshot.value as! [String:AnyObject]
+                let listOfFollowees = json["listOfFollowees"] as! [String]
+                print("is following these profs:")
+                print(listOfFollowees)
+                self.followeesArray = listOfFollowees
+            }
+            else {
+                // is following no one
+                print("user is not following anyone")
+            }
+        })
+        
+        getQuestionsAndAnswers() // great, now use this list to get the questions and answers
+    }
+    
+    
+    
+    
+    
+    func getQuestionsAndAnswers() {
+        questionArray = [] // reset question array
+        
+        //let questionsRef = FIRDatabase.database().reference(withPath: "questions")
+        let questionsRef = FIRDatabase.database().reference(withPath: "questions").queryOrdered(byChild: "date")
+        
+        questionsRef.observe(.childAdded, with: { snapshot in
+            
+            let json = snapshot.value as! [String:AnyObject]
+            
+            // check if following this prof, if so then put question in array to be displayed
+            if (self.followeesArray.contains(json["uid"] as! String)) {
+        
+                var questionData: [String:String] = [:]
+                
+                let question = json["question"]
+                let answer = json["answer"]
+                let uid = json["uid"]
+                let profName = json["profName"]
+                let date = json["date"]
+            
+                questionData["answer"] = answer as! String?
+                questionData["question"] = question as! String?
+                questionData["uid"] = uid as! String?
+                questionData["profName"] = profName as! String?
+                questionData["date"] = date as! String?
+                
+                self.questionArray.append(questionData as [String : AnyObject])
+            }
+            else {
+                print("user is NOT following this prof")
+            }
+            
+            print("printing question array")
+            print(self.questionArray)
+            print(self.questionArray.count)
+            self.tableView.reloadData()
+        })
     }
     
     func getProfessionalsList() {
@@ -89,26 +175,38 @@ class HomeVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
                 self.professionalArray.append(prof as [String : AnyObject])
             }
             
-            print(self.professionalArray)
+            //print(self.professionalArray)
             
         })
     }
     
     // number of rows in table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.questions.count
+        return self.questionArray.count
+        //return self.questions.count
     }
     
     // create a cell for each table view row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell:MyCustomCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! MyCustomCell
-
+        
+/*
         cell.myCellQuestion.text = self.questions[indexPath.row]
         cell.myCellAnswer.text = self.answers[indexPath.row]
         cell.myCellDate.text = self.dates[indexPath.row]
         cell.myCellTitle.text = "  " + self.titles[indexPath.row] + " answered a question"
-
+*/
+        
+        let thisQuestion = self.questionArray[indexPath.row]
+        let name = thisQuestion["profName"] as! String?
+        let question = thisQuestion["question"] as! String?
+        let answer = thisQuestion["answer"] as! String?
+        
+        cell.myCellQuestion.text = "Q: " + question!
+        cell.myCellAnswer.text = "A: " + answer!
+        cell.myCellDate.text = thisQuestion["date"] as! String?
+        cell.myCellTitle.text = " " + name! + " answered a question"
         
         return cell
     }
@@ -144,6 +242,8 @@ class HomeVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     }
 
     @IBAction func searchButton(_ sender: Any) {
+        // reset tableview arrays so can adjust to new data after (un)follows new profs
+        
         self.performSegue(withIdentifier: "searchProfessionals", sender: self)
     }
 
